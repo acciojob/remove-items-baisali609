f7e1e6a20e79979f0892d93fc00e47b37953e3c5
baisali609 <baisali609@gmail.com>
2025-02-14 12:58:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.querySelector("input").addEventListener("click", function () {
+    const select = document.getElementById("colorSelect"); 
+    const index = select.index; 
+
+    select.remove(index); 
+
+});
